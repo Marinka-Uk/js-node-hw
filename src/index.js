7f00cf39aa7172ac4ac154dd62e935247af3433d
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-07-15 19:11:20 +0300

    .

--- src/index.js
+++ src/index.js
@@ -5,4 +5,31 @@ const keysArray = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'o', 'p', 'i' ];
 let currentKeyIndex = 0; 
 keysArray[5]
 console.log(keysArray);
-document.getElementById('key').textContent = keysArray[currentKeyIndex]
+const keyElement = document.getElementById('key');
+keyElement.getElementById('key').textContent = keysArray[currentKeyIndex]
+function updateKey(){
+   keyElement.textContent = keysArray[currentKeyIndex]
+}
+
+document.addEventListener('keydown', (event) => {
+    if(event.key.toUpperCase() === keysArray[currentKeyIndex]){
+        currentKeyIndex = (currentKeyIndex + 1) % keysArray.length;
+        updateKey()
+    } 
+    else { 
+        PNotify.error({
+        title:'помилка',
+        text: `неправильна клавіша: ${event.key.toUpperCase()}`
+    });
+}
+});
+
+document.addEventListener('keypress', (event)=>{
+event.preventDefault();
+
+});
+
+document.getElementById('newGameBtn').addEventListener('click', ()=>{
+    currentKeyIndex = 0;
+    updateKey();
+});
